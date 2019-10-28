db6efff5a07a159bf34b5c69ab1a89c50e00794d
UnknownMonk <smodcartoon@gmail.com>
2019-10-28 14:15:54 -0400

    sutff

--- V_kata.js
+++ V_kata.js
@@ -1,1 +1,18 @@
-//new stuff
+    function wordsToMarks(string){
+       let charNum = []
+	    let spString = string.split('')
+      for(let i = 0; i < spString.length; i++){
+
+	    charNum.push(string.charCodeAt(i)-96)
+
+     }
+            return charNum.reduce((a,b) => a + b )
+    }
+
+
+	console.log(	wordsToMarks('string'))
+
+
+
+
+
