8d7d26bf0158241dd160dd29692cf3920da17c09
UnknownMonk <smodcartoon@gmail.com>
2019-10-31 08:35:23 -0400

    multiples

---
+++ multiplesOf3or5.js
@@ -0,0 +1,27 @@
+  function solution(number){
+   const sum = 0
+   for(let i = 3; i < number; i++){
+   if(i % 3 == 0 || i % 5 = 0){
+      sum += i
+
+   }
+   
+   }  
+    return sum;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
