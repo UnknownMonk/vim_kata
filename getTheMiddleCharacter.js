864893e820f7ce5fc5a5320e660acaae8a79c074
UnknownMonk <smodcartoon@gmail.com>
2019-11-08 10:13:20 -0500

    voule count kata

--- getTheMiddleCharacter.js
+++ getTheMiddleCharacter.js
@@ -12,3 +12,15 @@ console.log(getMiddle("test"), "es");
 console.log(getMiddle("testing"), "t");
 console.log(getMiddle("middle"), "dd");
 console.log(getMiddle("A"), "A");
+
+function findOutlier(integers){
+  const even = integers.filter(int => int % 2 === 0);
+  const odd  = integers.filter(int => int % 2 !== 0);
+  return even.length === 1 ? even[0] : odd[0];
+}
+
+console.log(findOutlier([0, 1, 2]), 1);
+console.log(findOutlier([1, 2, 3]), 2);
+console.log(findOutlier([2, 6, 8, 10, 3]), 3);
+console.log(findOutlier([0, 0, 3, 0, 0]), 3);
+console.log(findOutlier([1, 1, 0, 1, 1]), 0);
---
+++ vowelCound.js
@@ -0,0 +1,32 @@
+  function getCount(str) {
+  var vowelsCount = 0;
+  
+  
+   let splitSt = str.split('');
+   for(let i = 0; i < splitSt.length; i++){
+   if(splitST[i] == a ||  splitST[i] == e || splitST[i] == i || splitST[i] == o || splitST[i] == u){
+   vowelsCount++
+
+
+   }
+   } 
+  return vowelsCount;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
